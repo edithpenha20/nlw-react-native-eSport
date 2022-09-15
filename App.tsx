27bb43867422c8002f8356dc682c54c67ba04aaa
Penha <edith.penha@capgemini.com>
2022-09-15 18:14:04 -0300

    Consumo da API

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter';
 
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 import { Background } from './src/components/Background';
 import { Loading } from './src/components/Loading';
 
@@ -20,7 +20,7 @@ export default function App() {
         translucent
       />
 
-      { fontsLoader ? <Home /> : <Loading />}
+      { fontsLoader ? <Routes /> : <Loading />}
       
     </Background>
   );
